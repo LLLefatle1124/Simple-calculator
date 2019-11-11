df19b1bf26db2be05895467ac2a424f3f18f03ce
NoloLee <lawrence.lefatle@gmail.com>
2019-11-11 17:26:08 +0200

    god

--- spec/test_calculator.spec.js
+++ spec/test_calculator.spec.js
@@ -16,19 +16,13 @@ describe("add_numbers.", function(){
     expect(part2.add(0,0)).toBe(0)
     })
 
-    // let part2 = new calculator.myCalc()
-
     it("should add two negative numbers and return the sum.", function(){
         expect(part2.add(-1, -1)).toBe(-2)
     })
 
-    // let part2 = new calculator.myCalc()
-
     it("should add two negative numbers and return the sum.", function() {
         expect(part2.add(4, 5)).toBe(9)
     })
-
-    // let part2 = new calculator.myCalc()
 
     it("adds any count of numbers and returns the sum.", function(){
         expect(part2.add(1, 2, 3, 4)).toBe(10)
