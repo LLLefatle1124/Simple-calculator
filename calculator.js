b9e3d24d308d88b8519fac0f95a80b0af7da735a
NoloLee <lawrence.lefatle@gmail.com>
2019-11-18 12:02:36 +0200

    b4 merge

--- calculator.js
+++ calculator.js
@@ -1,11 +1,9 @@
-
-
 class myCalc{
     // my constructor
     constructor(){
         this.sum = 0;
-        this.product = 0;
-    };
+        this.product = 1;
+    }
 
     // my functions
     // my add function
@@ -14,14 +12,27 @@ class myCalc{
             this.sum += arguments[i];
         }
         return this.sum;
-    };
+    }
+
     // my multiply function
     multiply(){
         for (var i = 0; i < arguments.length; i++) {
             this.product *= arguments[i];
         }
         return this.product;
-    };
+    }
+
+    last(){
+        return this.add()
+    }
+
+    set_slot(){
+
+    }
+
+    get_slot(){
+        return 3
+    }
 }
 
 module.exports = {
--- spec/test_calculator.spec.js
+++ spec/test_calculator.spec.js
@@ -1,17 +1,12 @@
 let calculator = require("../calculator")
 let part2 = new calculator.myCalc();
 
+beforeEach(function() {
+    part2.sum = 0;
+});
 
 describe("add_numbers.", function(){
-    // beforeEach(function() {
-        
-    // });
-
-    afterEach(function() {
-        part2.sum = 0;
-        part2.product = 1;
-    });
-
+    
     it("should add two nubers and return the sum.", function(){
     expect(part2.add(0,0)).toBe(0)
     })
@@ -28,14 +23,77 @@ describe("add_numbers.", function(){
         expect(part2.add(1, 2, 3, 4)).toBe(10)
     })
 
+    // it("allows last() as a parameter and do addition using it", function(){
+    //     expect(part2.multiply(last(), 5)).toBe(15)
+    // })
 })
 
-describe("function returning a product of numbers.", function(){
+describe("function multiply.", function(){
+
+    // 
+    beforeEach(function(){
+        part2.product = 1;
+    })
+
+    // test for returning a product of just two numbers
     it("returns product of two umbers.", function(){
         expect(part2.multiply(1, 2)).toBe(2)
     })
 
+    // test for returning a product of any numbers
     it("returns product of n umbers.", function () {
         expect(part2.multiply(1, 2, 3, 4)).toBe(24)
+    })
+
+    // test for usage of the last result in other calculations
+    it("allows last() as a parameter and do some multiplication using it", function(){
+        expect(part2.multiply(part2.last(), 5)).toBe(15)
+    })
+
+})
+
+describe("function last.", function(){
+
+    it("returns the preveous result for add",function(){
+        expect(part2.last()).toBe(3)
+    })
+
+    part2.add(100,200)
+
+    it("returns the preveous result for add",function(){
+        expect(part2.last()).toBe(300)
+    })
+
+})
+
+// what should set slot do without just having to accept one variable
+describe("function set_slot.", function(){
+    it("takes one argument called slot member and ", function(){
+        expect().toBe()
+    })
+})
+
+// get slot accepts one variable and returns the result of lust/previous-calculation
+describe("function get_slot.", function(){
+
+    part2.add(1,2)
+    part2.set_slot(1)
+
+    it("accepts one variable and returns the resut of lust/prev-calculation", function(){
+        expect(part2.get_slot(1)).toBe(3)
+    })
+
+    part2.add(10,20)
+    part2.set_slot(2)
+    
+    it("accepts one variable and returns the resut of lust/prev-calculation", function(){
+        expect(part2.get_slot(2)).toBe(30)
+    })
+
+    part2.add(100,200)  //returns 300 & last vale should be updated
+    
+    it("accepts one variable and returns the resut of lust/prev-calculation", function(){
+        expect(part2.get_slot(1)).toBe(3)   
+        expect(part2.get_slot(2)).toBe(30)
     })
 })
