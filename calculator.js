d49fa743a230d8f135cf188a1080a36037bc9289
NoloLee <lawrence.lefatle@gmail.com>
2019-11-19 15:29:47 +0200

    all specs working

--- calculator.js
+++ calculator.js
@@ -1,33 +1,62 @@
 
- class myPart2{
+ class calculator{
+    
     // my Constructor
     constructor(){
-        
+    
+        this.slots = []
+        this.myLast = 0
+    
     }
 
     // My add function
     add(){
+
         this.sum = 0;
 
         for (var i = 0; i < arguments.length; i++) {
-            this.sum += arguments[i];
+            this.sum += arguments[i]
         }
-
-        return this.sum;
+        
+        this.myLast = this.sum
+        return this.sum
+    
     }
 
     // My multiply function
     multiply(){
-        this.product = 1;
+
+        this.product = 1
 
         for (var i = 0; i < arguments.length; i++) {
-            this.product *= arguments[i];
+            this.product *= arguments[i]
         }
+    
+        this.myLast = this.product
+        return this.product
+    
+    }
 
-        return this.product;
+    last(){
+    
+        return this.myLast
+   
+    }
+
+    set_slot(mySlot){
+   
+        this.slots[mySlot] = this.last()
+        return this.slots[mySlot]
+   
+    }
+
+    get_slot(_mySlot){
+    
+        return this.slots[_mySlot]
+    
     }
 }
 
 module.exports = {
-    myPart2
+    calculator
 }
